7f6cff290ff5b8a9e29a72fd3b33b930bfcd29a7
MireyaCR <mireya.mcr@gmail.com>
2022-10-18 21:37:47 +0000

    reacttodolist

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,44 @@
-import React from "react";
+
+import React, { useState } from 'react';
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
+//create the array
+
+
+// {tarea.map((element, index) => {
+// 	return (         			 
+// 		<li className={element}>texto de la tarea</li>		
+// 	)});
+
+
 const Home = () => {
+	const [tareas, setTareas] = useState([]);
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<input type="text" placeholder='What needs to be done?' onKeyDown={(event)=>{
+				if (event.key === 'Enter') {
+					setTareas([...tareas, event.target.value])
+					event.target.value=""
+				}
+			}} />
+			<ul className="list-group flush">
+				{tareas.map((element, index) => <li key={index+'li'} className="list-group-item"
+				onMouseEnter={(event)=>{
+					event.target.childNodes[1].style.visibility="visible"
+				}}
+				onMouseLeave={(event)=>{
+					event.target.childNodes[1].style.visibility="hidden"
+				}}>{element}
+				<button key={index} onClick={(event)=>{
+					tareas.splice(index,1)
+					setTareas([...tareas])
+				}}>x</button>
+				</li>)}	
+			</ul>
 		</div>
-	);
-};
+)};
+
 
 export default Home;
