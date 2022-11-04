adc51d05c728f6aa5d77b36a3102b736037ed4d4
MireyaCR <mireya.mcr@gmail.com>
2022-11-04 19:45:54 +0000

    todolist

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -7,29 +7,33 @@ import rigoImage from "../../img/rigo-baby.jpg";
 const Home = () => {
 	const [tareas, setTareas] = useState([]);
 	return (
-		<div className="container h-25 mt-5 p-5">
-			<h3>Tasks</h3>
-			<input className="form-control sm mb-3"type="text" placeholder='What needs to be done?' onKeyDown={(event)=>{
+		<div className="container bg-success bg-opacity-10 h-25 mt-4 p-5 w-50 rounded">
+			<h3 className="text-center text-muted">Tasks</h3>
+			<div className="border border-light border-bottom-0 mt-0 rounded">
+			<input className="form-control-md w-100 border-0 bg-dark bg-opacity-10 text-muted rounded" type="text" placeholder='What needs to be done?' onKeyDown={(event)=>{
 				if (event.key === 'Enter') {
 					setTareas([...tareas, event.target.value])
 					event.target.value=""
 				}
 			}} />
 			<ul className="list-group list-group-flush">
-				{tareas.map((element, index) => <li key={index+'li'} className="list-group-item"
+				{tareas.map((element, index) => <li key={index+'li'} className="list-group-item bg-light bg-opacity-10"
 				onMouseEnter={(event)=>{
 					event.target.childNodes[1].style.visibility="visible"
 				}}
 				onMouseLeave={(event)=>{
 					event.target.childNodes[1].style.visibility="hidden"
 				}}>{element}
-				<button className="btn btn-outline-danger btn-close" key={index} onClick={(event)=>{
+				<button type="button" className="btn-close float-end" key={index} onClick={(event)=>{
 					tareas.splice(index,1)
 					setTareas([...tareas])
-				}}></button>
+				}}> </button>
 				</li>)}	
 			</ul>
-			<p><small>{tareas.length} Task</small></p>
+			</div>
+				<div className="bg-light bg-opacity-10 border-light border-top shadow-sm mt-0 p-1 rounded"><small className="mx-1">{tareas.length} Task</small></div>
+				<div className="bg-light bg-opacity-10 border-light shadow-sm mt-0 p-1 mx-2 rounded"></div>
+				<div className="bg-light bg-opacity-10 border-light shadow-sm mt-0 p-1 mx-3 rounded"></div>			
 		</div>
 )};
 
